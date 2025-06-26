c29c0747d664bca4d9339efdaf00a104d8a61d7a
bazuca0001 <rodrigolokogamer@gmail.com>
2025-06-26 04:34:52 -0300

    Update video.py

--- src/telegram/_files/video.py
+++ src/telegram/_files/video.py
@@ -4,8 +4,10 @@
 # Copyright (C) 2015-2025
 # Leandro Toledo de Souza <devs@python-telegram-bot.org>
 #
+https://github.com/91DarioDev/ForwardsCoverBot
+
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
+# it under the terms of j GNU Lesser Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
